402bbadfd63e410f9ffa9f28fee93dcb943afdb6
AbhayKaushik <abhay.gyanbharati@gmail.com>
2023-04-09 12:12:25 +0530

    Add intermodule communication for Monitoring Service using Kafka
    - Instead of using API endpoints to check if the various system
      services are running or not, we will be using Kafka and pushing
      the healthcheck request into the topics of each system service.
    
      The topics list will be retrieved from topic_info.json file.
    
    - Added multithreading in MonitoringServices.py to push into the
      topics at a given frequency and always listen to the monitoring
      topic.

--- MonitoringService/MonitoringService.py
+++ MonitoringService/MonitoringService.py
@@ -19,62 +19,22 @@ MONITORING SERVICE:
 import requests
 import json
 
+import time
+import sys
+import threading
+from argparse import ArgumentParser, FileType
+from configparser import ConfigParser
+from confluent_kafka import Consumer, OFFSET_BEGINNING
+from confluent_kafka import Producer
+
 
 def getServiceInfo(filePath: str) -> dict:
-    """get the service info from the Bootstrap Module"""
-    # For now, we will get the service info from dummy service info file
+    """get the list of topics of the various submodules from the Bootstrap Module"""
+    # For now, we will get the topic info from dummy topic info file
     with open(filePath, "r") as f:
         response = json.load(f)
 
     return response
-
-
-def getHealthStatus(IP: str, PORT: str) -> bool:
-    api_url = f"http://{IP}:{PORT}/healthcheck"
-    # get the response from the module at IP:PORT
-    try:
-        response = requests.get(api_url)
-        # print(response.json())
-        status = response.json()["status"]
-    except Exception as e:
-        # print(e)
-        # refused to connect => no response
-        status = False
-
-    return status
-
-
-def healthCheck(service_info: dict) -> None:
-    """Iterate through the global list containing the IP:PORT of
-    each service and get the health status of each service"""
-
-    for service_name, socket_address in service_info.items():
-        IP = socket_address["IP"]
-        PORT = socket_address["PORT"]
-        status = getHealthStatus(IP, PORT)
-
-        if status == False:
-            # the service is down
-            print(f"{service_name}: [DOWN]")
-            # contact someone to try restart the service
-            # if not then run a new instance
-            report = {
-                "serviceName": service_name,
-                "status": False,
-            }
-            headers = {"Content-Type": "application/json"}
-
-            # get the IP and PORT of NodeManager
-            socket_address = service_info["NodeManager"]
-            IP = socket_address["IP"]
-            PORT = socket_address["PORT"]
-
-            api_url = f"http://{IP}:{PORT}/contact"
-            response = requests.post(api_url, json=report)
-            print(response.json())
-        else:
-            # the service is up
-            print(f"{service_name}: [UP]")
 
 
 def getTrafficLogs(service_info) -> dict:
@@ -115,12 +75,126 @@ def extractQoSValues(traffic_logs: dict) -> None:
         print(f"Reponse Time for {microservice_name}: ")
         print(response_times)
 
+        # TODO: Store the Qos Values in the database
+
+
+# def getQoSValues() -> dict:
+#     """Send QoS values"""
+#     report = {
+#         "serviceName": microservice_A,
+#         "QoS": 2.0341,
+#     }
+#     headers = {"Content-Type": "application/json"}
+#
+#     # get the IP and PORT of NodeManager
+#     socket_address = service_info["APIManager"]
+#     IP = socket_address["IP"]
+#     PORT = socket_address["PORT"]
+#
+#     api_url = f"http://{IP}:{PORT}/sendQoSVals"
+#     response = requests.post(api_url, json=report)
+#     print(response.json())
+
+
+def healthCheck(config, frequency) -> None:
+    # Create Producer instance
+    producer = Producer(config)
+
+    # Optional per-message delivery callback (triggered by poll() or flush())
+    # when a message has been successfully delivered or permanently
+    # failed delivery (after retries).
+    def delivery_callback(err, msg):
+        if err:
+            print(f"ERROR: Message failed delivery: {err}")
+        else:
+            print(f"Healthcheck sent to {msg.topic()}")
+            # print(
+            #     "Produced event to topic {topic}: key = {key:12} value = {value:12}".format(
+            #         topic=msg.topic(),
+            #         key=msg.key().decode("utf-8"),
+            #         value=msg.value().decode("utf-8"),
+            #     )
+            # )
+
+    services = getServiceInfo("./topic_info.json")
+
+    while True:
+        time.sleep(frequency)
+        for service_name, service_data in services.items():
+            producer.produce(
+                service_data["topic_name"],
+                "",
+                "healthcheck",
+                on_delivery=delivery_callback,
+            )
+
+        print("Healthcheck done")
+
+        # Block until the messages are sent.
+        producer.poll(10)
+        producer.flush()
+
+
+def getHealthStatus(config) -> None:
+    # Set up a callback to handle the '--reset' flag.
+    def reset_offset(consumer, partitions):
+        if args.reset:
+            for p in partitions:
+                p.offset = OFFSET_BEGINNING
+            consumer.assign(partitions)
+
+    # Create Consumer instance
+    consumer = Consumer(config)
+
+    # Subscribe to topic
+    topic = "topic_monitoring"
+    consumer.subscribe([topic], on_assign=reset_offset)
+
+    # Poll for new messages for the Monitoring topic
+    while True:
+        msg = consumer.poll(1.0)
+        if msg is None:
+            # Initial message consumption may take up to
+            # `session.timeout.ms` for the consumer group to
+            # rebalance and start consuming
+            print("Waiting...")
+        elif msg.error():
+            print("ERROR: %s".format(msg.error()))
+        else:
+            # Extract the (optional) key and value, and print.
+            print(msg)
+            print(f"{msg.topic()}: [UP]")
+            # print("Consumed event from topic {topic}: key = {key:12} value = {value:12}".format(
+            #     topic=msg.topic(), key=msg.key().decode('utf-8'), value=msg.value().decode('utf-8')))
+
 
 if __name__ == "__main__":
-    service_info = getServiceInfo("./dummyBoostrapResponse.json")
-    healthCheck(service_info)
-    # traffic_logs = getTrafficLogs(service_info)
-    with open("./dummyTrafficLogs.json") as f:
-        traffic_logs = json.load(f)
+    # Parse the command line.
+    parser = ArgumentParser()
+    parser.add_argument("config_file", type=FileType("r"))
+    parser.add_argument("--reset", action="store_true")
+    args = parser.parse_args()
 
-    extractQoSValues(traffic_logs)
+    # Parse the configuration.
+    # See https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
+    config_parser = ConfigParser()
+    config_parser.read_file(args.config_file)
+    config = dict(config_parser["default"])
+    config.update(config_parser["consumer"])
+
+    # healthCheck(config)
+    # getHealthStatus(config)
+    frequency = 5
+
+    # Create a producer to send healthcheck request at regular intervals
+    # and update the timeout queue
+    producer_thread = threading.Thread(target=healthCheck, args=(config, frequency))
+
+    # Create a consumer to consume the message and update the timeout queue
+    consumer_thread = threading.Thread(target=getHealthStatus, args=(config,))
+
+    # TODO: Create a timeout tracker to keep track of the values in the timeout queue
+
+    # start the two threads
+    producer_thread.start()
+    consumer_thread.start()
--- MonitoringService/README.md
+++ MonitoringService/README.md
@@ -1,11 +1,12 @@
 ## MonitoringService
 
 * To Execute the code :
-1. run dummyAPI file 
-    $ pipenv run uvicorn dummyAPI:app --reload
-
 2. run MonitoringService.py
-    $ pipenv run MonitoringService.py
+```
+pipenv run python3 newMonitoringService.py ../kafka_setup.ini
+```
 
 3. run MonitoringNodes.py
-    $ pipenv run MonitoringNodes.py
+```
+pipenv run MonitoringNodes.py
+```
