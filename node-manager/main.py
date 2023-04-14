37262f437f8952519609cd14e95b2dc7fcbd2534
bhanujggandhi <gandhibhanuj@gmail.com>
2023-04-15 01:27:30 +0530

    docker file optimised

--- node-manager/main.py
+++ node-manager/main.py
@@ -22,13 +22,16 @@ producer = Produce()
 
 
 def generate_docker_image(service):
-    s = """FROM python:3.9
+    s = """FROM python:3.10-slim-bullseye
 
-COPY . /app/
+RUN apt-get update && apt-get install -y --no-install-recommends --no-install-suggests build-essential && pip install --no-cache-dir --upgrade pip
 
 WORKDIR /app
+COPY ./requirements.txt /app
+RUN pip install --no-cache-dir --requirement /app/requirements.txt
+COPY . /app
 
-RUN pip install -r requirements.txt
+EXPOSE 80
 
 CMD ["python3", "main.py"]"""
 
