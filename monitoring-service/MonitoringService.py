c90ebee256819dc219219ae30c12aab44f62c885
Mohit Sharma <sharmaji@Mohits-Air.mshome.net>
2023-04-19 01:00:15 +0530

    Monitoring service updated
    Implemented hearbeat method, tracking of modules, not fully tested

--- monitoring-service/MonitoringService.py
+++ monitoring-service/MonitoringService.py
@@ -3,198 +3,256 @@ MONITORING SERVICE:
     * Gets initalized with a config file describing:
         - The rules for determining threshold
 
-    * Will monitor the levels of CPU and memory usage for each instance of the 
-    node in the node manager. It will use the rules in the config file to inform
-    the node manager in case there is need of upscaling and downscaling.
-
-    * Get the health status of all platform modules by hitting their healthcheck
-    API.
-
-    * Get the metrics of all services from the response time logs created by API Manager
-
     Assumes:
     * Node Manager is always up
     * API Manager is up when traffic data is asked
+
+    INFORMATION
+
+    * MESSAGE format to sent : {"receiver_module": moduleName,"sender_module":"Moniotring service","remark": "Healthcheck" }
+    * MESSAGE format to receive : {"receiver_module": "Moniotring service","sender_module":"Your_module_name","time_stamp": time.time() }
+    * API format for modules which doen't use Kafka : /ping/{module_name}
 """
 import requests
 import json
-
 import time
-import sys
+from decouple import config
+from pymongo import MongoClient
 import threading
-from argparse import ArgumentParser, FileType
-from configparser import ConfigParser
-from confluent_kafka import Consumer, OFFSET_BEGINNING
-from confluent_kafka import Producer
+from Messenger import Produce, Consume
 
+PRODUCER_SLEEP_TIME = 3 
+CONSUMER_SLEEP_TIME = 1
+API_SLEEP_TIME = 5 
+MAIN_SLEEP_TIME = 1 
+TRACKING_INTERVAL = 10  
+TIMEOUT_THRESHOLD = 30  
+IP = "127.0.0.1"
+PORT = "8000"
+MY_TOPIC="topic_monitoring"
+MODULE_LIST=[]
+API_MODULE_LIST = []
+CONFIG_FILE_PATH = "./topic_info.json"
 
-def getServiceInfo(filePath: str) -> dict:
+# Establish connection to MongoDB
+mongokey = config('mongoKey')
+client = MongoClient(mongokey)
+db = client["platform"]
+collection = db["Module_status"]
+# app_collection = db["AppCollection"]
+
+# Load topic info from configuration file
+def get_service_info(config_file):
     """get the list of topics of the various submodules from the Bootstrap Module"""
-    # For now, we will get the topic info from dummy topic info file
-    with open(filePath, "r") as f:
-        response = json.load(f)
-
-    return response
-
-
-def getTrafficLogs(service_info) -> dict:
-    """Get the traffic logs from the API Manager module"""
-
-    # get the IP and PORT of APIManager
     try:
-        socket_address = service_info["APIManager"]
-        IP = socket_address["IP"]
-        PORT = socket_address["PORT"]
-    except Exception as e:
-        print(f"{e} is not in list of services")
-        return dict()
+        with open(config_file, 'r') as f:
+            response = json.load(f)
+            # Extract the keys and store them in a list
+            Module_list = list(response.keys())
+            return response,Module_list
+    except FileNotFoundError as e:
+        print(f"Configuration file not found: {config_file}")
+        return {},[]
+    except json.JSONDecodeError as e:
+        print(f"Failed to decode configuration file: {config_file}. Error: {e}")
+        return {},[]
 
-    api_url = f"http://{IP}:{PORT}/traffic_logs"
-    # get the response from the module at IP:PORT
+# **********************************|    Dealing with Mongo    |***********************************
+
+# Function to store module health status in MongoDB
+def store_health_status(module_name, timestamp):
     try:
-        response = requests.get(api_url)
-        return response
-
+        # Create a document with the module name and timestamp
+        filter = {'module_name': module_name}
+        # Define the update values
+        update = {
+            '$set': {
+                'status': 'active',
+                'last_updated': timestamp
+            }
+        }
+        collection.update_one(filter, update, upsert=True)
     except Exception as e:
-        print(e)
-        return dict()
+        print(f"Error: {e}. Failed to store health status for module '{module_name}'")
 
 
-def extractQoSValues(traffic_logs: dict) -> None:
-    for microservice_name, logs in traffic_logs.items():
-        requests = logs["requests"]
-        responses = logs["responses"]
-
-        total = min(len(requests), len(responses))
-
-        response_times = []
-
-        for query in range(total):
-            response_times.append(responses[query] - requests[query])
-
-        print(f"Reponse Time for {microservice_name}: ")
-        print(response_times)
-
-        # TODO: Store the Qos Values in the database
-
-
-# def getQoSValues() -> dict:
-#     """Send QoS values"""
-#     report = {
-#         "serviceName": microservice_A,
-#         "QoS": 2.0341,
-#     }
-#     headers = {"Content-Type": "application/json"}
-#
-#     # get the IP and PORT of NodeManager
-#     socket_address = service_info["APIManager"]
-#     IP = socket_address["IP"]
-#     PORT = socket_address["PORT"]
-#
-#     api_url = f"http://{IP}:{PORT}/sendQoSVals"
-#     response = requests.post(api_url, json=report)
-#     print(response.json())
-
-
-def healthCheck(config, frequency) -> None:
-    # Create Producer instance
-    producer = Producer(config)
-
-    # Optional per-message delivery callback (triggered by poll() or flush())
-    # when a message has been successfully delivered or permanently
-    # failed delivery (after retries).
-    def delivery_callback(err, msg):
-        if err:
-            print(f"ERROR: Message failed delivery: {err}")
+# Function to get last update timestamp of a specific module from MongoDB
+def get_last_update_timestamp(module_name):
+    try:
+        # Query MongoDB to get the latest document of the specific module based on module_name field
+        document = collection.find_one({"module_name": module_name})
+        # Extract and return the timestamp from the document
+        if document:
+            return document['last_updated']
         else:
-            print(f"Healthcheck sent to {msg.topic()}")
-            # print(
-            #     "Produced event to topic {topic}: key = {key:12} value = {value:12}".format(
-            #         topic=msg.topic(),
-            #         key=msg.key().decode("utf-8"),
-            #         value=msg.value().decode("utf-8"),
-            #     )
-            # )
+            return None
+    except Exception as e:
+        print(f"Error: {e}. Failed to get last update timestamp for module '{module_name}'")
+        return None
 
-    services = getServiceInfo("./topic_info.json")
+# Function to update module status in MongoDB
+def updateStatus(module_name):
+    try:
+        # Create a document with the module name and timestamp
+        filter = {'module_name': module_name}
+        # Define the update values
+        update = {
+            '$set': {
+                'status': 'inactive'
+            }
+        }
+        # app_collection.update_one(filter, update, upsert=True)
+    except Exception as e:
+        print(f"Error: {e}. Failed to update status for module '{module_name}'")
+
+
+# **********************************| Communication with Modules |***********************************
+
+def apiHealthCheck():
+    """
+    Health check of modules, which does not use Kafka, through API
+    """
+    while True:
+        for module_name in API_MODULE_LIST:
+            """need to change this api_url according to api provided by the module for health check"""
+            api_url = f"http://{IP}:{PORT}/ping/{module_name}"
+        
+            try:
+                response_dict = requests.get(api_url).json()
+                timestamp = response_dict["time_stamp"]
+                # Store module health status in MongoDB
+                store_health_status(module_name, float(timestamp))
+                return response_dict
+        
+            except Exception as e:
+                print(e)
+                return dict()
+            
+        time.sleep(API_SLEEP_TIME)
+   
+def postHealthCheck():
+    """
+    post health messages to all the module's
+    """
+    print("Post health check messages started... ")
+    services,MODULE_LIST=get_service_info(CONFIG_FILE_PATH)
+    # topic_names = [value["topic_name"] for value in services.values()]
+
+    if not MODULE_LIST:
+        print("Failed to get MODULE_LIST from configuration file. Aborting...")
+        return
+
+    produce = Produce()  # Instantiate Kafka producer
+    while True:
+        try:
+            for Module in MODULE_LIST :
+                key = ""
+                topic_name = services[Module]["topic_name"]
+
+                # needs to decide what message format will be..
+                message = {"receiver_module": Module,"sender_module":"Moniotring service","remark": "Healthcheck" }
+                produce.push(topic_name, key, json.dumps(message))
+        except KeyError as e:
+            print(f"KeyError: {e}. Failed to post health check message.")
+        except Exception as e:
+            print(f"Error: {e}. Failed to post health check message.")
+        # time interval for next health message to send.    
+        finally:
+            time.sleep(PRODUCER_SLEEP_TIME)
+
+def getHealthStatus():
+    """
+    Get health status from Kafka topic
+    """
+    print("Get health status started... ")
+    consume = Consume(MY_TOPIC)
 
     while True:
-        time.sleep(frequency)
-        for service_name, service_data in services.items():
-            producer.produce(
-                service_data["topic_name"],
-                "",
-                "healthcheck",
-                on_delivery=delivery_callback,
-            )
+        try:
+            # Get messages from Kafka topic
+            resp = consume.pull()
+            if resp["status"] == False:
+                print(resp["value"])
+            else:
+                # print(resp["key"], resp["value"])
+                value = json.loads(resp["value"])
+                # Extract module name and timestamp from the message
+                module_name = value.get("sender_module")
+                timestamp = value.get("time_stamp")
 
-        print("Healthcheck done")
+                # Check if module_name and timestamp are present in the message
+                if module_name is not None and timestamp is not None:
+                    # Store module health status in MongoDB
+                    store_health_status(module_name, float(timestamp))
+                else:
+                    print("Error: Required fields are missing in the health status message.")
+        except KeyError as e:
+            print(f"Error: {e}. Failed to get health status.")
+        except Exception as e:
+            print(f"Error: {e}. Failed to get health status.")
+        finally:
+            time.sleep(CONSUMER_SLEEP_TIME)
 
-        # Block until the messages are sent.
-        producer.poll(10)
-        producer.flush()
 
-
-def getHealthStatus(config) -> None:
-    # Set up a callback to handle the '--reset' flag.
-    def reset_offset(consumer, partitions):
-        if args.reset:
-            for p in partitions:
-                p.offset = OFFSET_BEGINNING
-            consumer.assign(partitions)
-
-    # Create Consumer instance
-    consumer = Consumer(config)
-
-    # Subscribe to topic
-    topic = "topic_monitoring"
-    consumer.subscribe([topic], on_assign=reset_offset)
-
-    # Poll for new messages for the Monitoring topic
+# **********************************| Monitoring of Modules |***********************************
+ 
+def timeOutTracker():
+    """
+    Method that keep track of all the modules to be monitored
+    """
+    print("Tracker to monitor modules started....")
     while True:
-        msg = consumer.poll(1.0)
-        if msg is None:
-            # Initial message consumption may take up to
-            # `session.timeout.ms` for the consumer group to
-            # rebalance and start consuming
-            print("Waiting...")
-        elif msg.error():
-            print("ERROR: %s".format(msg.error()))
-        else:
-            # Extract the (optional) key and value, and print.
-            print(msg)
-            print(f"{msg.topic()}: [UP]")
-            # print("Consumed event from topic {topic}: key = {key:12} value = {value:12}".format(
-            #     topic=msg.topic(), key=msg.key().decode('utf-8'), value=msg.value().decode('utf-8')))
+        try:
+            # Check if any module has not responded for a long time
+            current_timestamp = time.time()
+
+            # Iterate through the list of modules
+            for module_name in MODULE_LIST:
+                try:
+                    last_update_timestamp = get_last_update_timestamp(module_name)
+
+                    if last_update_timestamp and (current_timestamp - last_update_timestamp) > TIMEOUT_THRESHOLD:
+                        # Take action and send notification to admin
+                        print(f"Module '{module_name}' has not responded for a long time! Notification sent to admin..")
+                        updateStatus(module_name)
+                        """
+                        TO DO : some more action when required
+                        """
+                except Exception as e:
+                    print(f"Error: {e}. Failed to monitor module '{module_name}' for timeout.")
+                    # Continue to the next module in case of any error
+                    continue
+
+            # Sleep for a specific interval
+            time.sleep(TRACKING_INTERVAL)
+
+        except Exception as e:
+            print(f"Error: {e}. Failed to monitor modules to track .")
+            continue
 
 
 if __name__ == "__main__":
-    # Parse the command line.
-    parser = ArgumentParser()
-    parser.add_argument("config_file", type=FileType("r"))
-    parser.add_argument("--reset", action="store_true")
-    args = parser.parse_args()
-
-    # Parse the configuration.
-    # See https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
-    config_parser = ConfigParser()
-    config_parser.read_file(args.config_file)
-    config = dict(config_parser["default"])
-    config.update(config_parser["consumer"])
-
-    # healthCheck(config)
-    # getHealthStatus(config)
-    frequency = 5
-
-    # Create a producer to send healthcheck request at regular intervals
-    # and update the timeout queue
-    producer_thread = threading.Thread(target=healthCheck, args=(config, frequency))
+    # Create a producer to send healthcheck request at regular intervals and update the timeout queue
+    producer_thread = threading.Thread(target=postHealthCheck, args=())
 
     # Create a consumer to consume the message and update the timeout queue
-    consumer_thread = threading.Thread(target=getHealthStatus, args=(config,))
+    consumer_thread = threading.Thread(target=getHealthStatus, args=())
 
-    # TODO: Create a timeout tracker to keep track of the values in the timeout queue
+    #Create a timeout tracker to keep track of the values in the timeout queue
+    tracker_thread = threading.Thread(target=timeOutTracker, args=())
 
-    # start the two threads
+    # start the all the  threads.
     producer_thread.start()
     consumer_thread.start()
+    tracker_thread.start()
+
+    try:
+        # Keep the main thread running
+        while True:
+            time.sleep(MAIN_SLEEP_TIME)
+    except KeyboardInterrupt:
+        # Gracefully terminate threads on keyboard interrupt
+        producer_thread.join()
+        consumer_thread.join()
+        tracker_thread.join()
+
