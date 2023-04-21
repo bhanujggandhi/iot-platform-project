62e1e67dedeebd8dced92fc332e9e9ee4a3cdb4f
Vaibhav Saxena <Vaibhav.1865@gmail.com>
2023-04-21 18:29:23 +0530

    added fetching data feature using sensorIDs

--- sensor-manager/remotecalls.py
+++ sensor-manager/remotecalls.py
@@ -2,7 +2,7 @@
 import requests
 from decouple import config
 from pymongo import MongoClient
-
+import datetime
 parametertoSensorIDAPI = "https://iudx-rs-onem2m.iiit.ac.in/resource/nodes/"
 
 
@@ -15,30 +15,148 @@ client = MongoClient(mongokey)
 
 def fetchdatahelper(sensorIDs, startTime):
     data = {}
-    print("Fetching data for ", sensorIDs, " from ", startTime)
+    # print("Fetching data for ", sensorIDs, " from ", startTime)
     for i in sensorIDs:
         res = requests.get(dataFetchAPI+i+"/feeds?start="+startTime)
         if res.status_code == 200:
             res = res.json()
-            data[i] = res["feeds"]
+            # feild names
+            descriptor = requests.get(descriptorAPI+i)
+            fields = []
+            if descriptor.status_code == 200:
+                descriptor = descriptor.json()
+                descriptor = descriptor["Data String Parameters"]
+                for j in descriptor:
+                    fields.append(j)
+                # print(descriptor)
+            if (len(fields) > 0):
+                # data[i] = res["feeds"]
+                data[i] = {
+                    "fields": fields,
+                    "data": res["feeds"]
+                }
+            else:
+                data[i] = {
+                    "fields": "Could not fetch fields",
+                    "data": res["feeds"]
+                }
+
         else:
-            data[i] = []
+            data[i] = {}
     return data
 
 
+def validateSensorIDs(sensorIDs):
+    for i in sensorIDs:
+        try:
+            res = requests.get(descriptorAPI+i, timeout=2)
+            if res.status_code != 200:
+                return False
+        except:
+            return False
+    return True
+
+
+def validateReadingType(readingType):
+    response = requests.get(parametertoSensorIDAPI)
+    if response.status_code == 200:
+        response = response.json()
+        results = response["results"]
+        if readingType in results:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+def validateStartTime(startTime):
+    if startTime[-1] != "Z":
+        startTime = startTime + "Z"
+    startTime = startTime.replace("Z", "")
+    startTime = startTime.replace("T", " ")
+    try:
+        datetime.datetime.strptime(startTime, '%Y-%m-%d %H:%M:%S')
+        return True
+    except ValueError:
+        return False
+
+
 def fetchdata(parms):
-    readingType = parms["readingtype"]
-    startTime = parms["starttime"]
-    numofSensor = parms["numofsensors"]
+    readingType = ""
+    latitute = ""
+    longitude = ""
+    SensorIDsbyUser = []
+    startTime = ""
+    numofSensor = 1
+    data_flag = True
+
+    if "sensorIDs" in parms and len(parms["sensorIDs"]) > 0:
+        SensorIDsbyUser = parms["sensorIDs"]
+
+    if readingType in parms and parms["readingtype"] != "":
+        readingType = str(parms["readingtype"])
+        if validateReadingType(readingType):
+            readingType = readingType
+        else:
+            return {"Error": "Invalid reading type"}
+
+    if "lat" in parms and "long" in parms and parms["lat"] != "" and parms["long"] != "":
+        latitute = str(parms["lat"])
+        longitude = str(parms["long"])
+
+    if startTime in parms and parms["starttime"] != "":
+        startTime = parms["starttime"]
+        if startTime[-1] != "Z":
+            startTime = startTime + "Z"
+        if validateStartTime(startTime):
+            startTime = startTime
+        else:
+            return {"Error": "Invalid start time"}
+
+    if numofSensor in parms:
+        if type(parms["numofsensors"]) == int:
+            if parms["numofsensors"] > 0:
+                numofSensor = parms["numofsensors"]
+            else:
+                return {"Error": "Invalid number of sensors"}
+        else:
+            return {"Error": "Invalid number of sensors"}
+
+    if "data_flag" in parms:
+        if type(parms["data_flag"]) == bool:
+            data_flag = parms["data_flag"]
+        else:
+            return {"Error": "Invalid data flag"}
+
+    if len(SensorIDsbyUser) > 0:
+        sensorIDs = SensorIDsbyUser
+        if validateSensorIDs(sensorIDs):
+            print("Valid sensorIDs")
+            if data_flag:
+                data = fetchdatahelper(sensorIDs, startTime)
+                return data
+            else:
+                return sensorIDs
+        else:
+            return {"Error": "Invalid sensorIDs"}
+
     sensorIDs = []
-    if "lat" in parms and "long" in parms:
-        lat = parms["lat"]
-        long = parms["long"]
+    if latitute != "" and longitude != "":
+        # lat = parms["lat"]
+        # long = parms["long"]
+        # print("Fetching sensorIDs for given lat long")
         db = client["SensorDB"]
         collection = db["LatLongtoSensorID"]
-        res = collection.find_one({"location": lat+","+long})
-        if res:
-            sensorIDs = res["sensorIDs"]
+        # print("Connected to DB")
+        # print("Searching for ", latitute+","+longitude , collection.count_documents({"location": latitute+","+longitude}))
+        if collection.count_documents({"location": latitute+","+longitude}) == 0:
+            print("No sensorIDs for given lat long")
+        else:
+            res = collection.find_one({"location": latitute+","+longitude})
+            if res:
+                sensorIDs = res["sensorIDs"]
+                print("Found sensorIDs for given lat long")
 
     # check if reading type some value
     if readingType != "":
@@ -58,19 +176,26 @@ def fetchdata(parms):
         return {"Error": "No sensor for given parameters"}
     if len(sensorIDs) > numofSensor:
         sensorIDs = sensorIDs[:numofSensor]
-    data = fetchdatahelper(sensorIDs, startTime)
-    return data
+    if data_flag:
+        data = fetchdatahelper(sensorIDs, startTime)
+        return data
+    else:
+        return sensorIDs
 
 
 def main():
     parms = {
-        "readingtype": "Flowrate",
+        # "readingtype": "Flowrate",
         "starttime": "2023-01-14T08:26:20Z",
-        "numofsensors": 10,
+        "numofsensors": 2,
         "lat": "17.445402",
-        "long": "78.349875"
+        "long": "78.349875",
+        # "sensorIDs": ["WM-ds-PH03-00"],
+        "data_flag": True
     }
     data = fetchdata(parms)
+    # with open("data.json", "w") as f:
+    #     f.write(str(data))
     print(data)
 
 
